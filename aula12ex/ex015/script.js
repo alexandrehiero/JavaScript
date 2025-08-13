eb498d1ce039de4db619172f5352f082afb4a8b9
Alexandre Hiero <alexandrehrvianna@gmail.com>
2025-08-13 17:20:55 -0300

    Evoluindo no exercício de condições

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -4,11 +4,24 @@ function calcular() {
 
     var anodigitado = window.document.getElementById('anoi')
     var anodigitado = Number(anodigitado.value)
-
     var idade = anoatual - anodigitado
-
-    var res = window.document.getElementById('res')
     
+    var sexo = window.document.getElementsByName('sexo')[0]
+
+    res.innerHTML = `Sua idade é de ${idade} anos.`
+
+
+    if (idade < 18) { 
+        img.src = 'imagens/homem_adolescente.jpg' 
+    } else if (idade <= 59) {
+        img.src = 'imagens/homem_adulto.jpg'
+    } else if (idade >= 60 && idade <= 110) {
+        img.src = 'imagens/homem_senhor.jpg'
+    } else {
+        res.innerHTML = `Sua idade é inválida!`
+    }
+
+
 }
 
 
