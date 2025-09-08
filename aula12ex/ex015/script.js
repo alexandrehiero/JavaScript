758abbe0f84347882ff6cb72b6af6c592ad45b97
Alexandre Hiero <alexandrehrvianna@gmail.com>
2025-09-08 19:22:11 -0300

    Finalização Exercícios de Condições

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,32 +1,55 @@
-function calcular() {
+function verificar() {
     var agora = new Date()
     var anoatual = agora.getFullYear()
     var anodigitado = window.document.getElementById('anoi')
     var anodigitado = Number(anodigitado.value)
-    var idade = anoatual - anodigitado
+    var res = window.document.getElementById('res')
+    var fundo = window.document.getElementById('tamanhodadiv')
 
-    var res = window.document.getElementById('texto')
-    res.innerHTML = `Você tem ${idade} anos.`
-
-    var tamanhodadiv = window.document.getElementById('tamanhodadiv')
-    tamanhodadiv.style.height = '350px'
-
-
-    var foto = window.document.getElementById('foto')
-    
-    if (idade < 18 && idade > 0) {
-        foto.src = 'imagens/homem_adolescente.jpg' 
-
-    } else if (idade <= 59) {
-        foto.src = 'imagens/homem_adulto.jpg'
-
-    } else if (idade >= 60 && idade <= 110) {
-        foto.src = 'imagens/homem_senhor.jpg'
-
+    if (anodigitado == 0 || anodigitado > anoatual || anodigitado < 0 ) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
     } else {
-        res.innerHTML = `Sua idade é inválida!`
-    }
+        var sexo = window.document.getElementsByName('sexo')
+        var idade = anoatual - anodigitado
+        var genero = ''
+        var img = window.document.createElement('img')
+        img.setAttribute('id', 'foto')
 
+        if (sexo[0].checked) {
+            genero = 'Homem'
+
+            if (idade >= 0 && idade <= 21) {
+                // Jovem
+                img.setAttribute('src', 'homem_adolescente.jpg')
+            } else if (idade > 21 && idade <= 59) {
+                // Adulto
+                img.setAttribute('src', 'homem_adulto.jpg')
+            } else if (idade > 60 && idade < 120) {
+                // Senhor
+                img.setAttribute('src', 'homem_senhor.jpg')
+            }
+
+        } else if (sexo[1].checked) {
+            genero = 'Mulher'
+
+            if (idade >= 0 && idade <= 21) {
+                // Jovem
+                img.setAttribute('src', 'mulher_jovem.jpg')
+            } else if (idade > 21 && idade <= 59) {
+                // Adulta
+                img.setAttribute('src', 'mulher_adulta.jpg')
+            } else if (idade > 60 && idade < 120) {
+                // Senhora
+                img.setAttribute('src', 'mulher_senhora.jpg')
+            }
+        }
+
+        res.style.textAlign = 'center'        
+        res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`      
+        res.appendChild(img)
+
+        fundo.style.height = '320px'
+    }
 }
 
 
